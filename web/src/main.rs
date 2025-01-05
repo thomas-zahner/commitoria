b3225842757fad4f6555b3310700f0d0d4b9199e
Thomas Zahner <thomas.zahner@protonmail.ch>
2025-01-05 18:51:50 +0100

    Call core function in web route

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,7 +1,15 @@
-use axum::{routing::get, Router};
+use axum::{http::StatusCode, routing::get, Json, Router};
+use commitoria_core::{
+    provider::{github::Github, GitProvider},
+    source::ReqwestDataSource,
+    ContributionActivity,
+};
 
-async fn get_calendar_data() -> &'static str {
-    "Hello, World!"
+async fn get_calendar_data() -> Result<Json<ContributionActivity>, StatusCode> {
+    match Github::fetch(ReqwestDataSource {}, "mre".into()).await {
+        Ok(r) => Ok(Json(r)),
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+    }
 }
 
 #[tokio::main]

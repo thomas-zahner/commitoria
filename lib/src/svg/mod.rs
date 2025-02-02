3b3287700a3099f2a68278bc8ec61d0dad065cd1
Thomas Zahner <thomas.zahner@protonmail.ch>
2025-02-02 17:19:29 +0100

    Render day cells

--- lib/src/svg/mod.rs
+++ lib/src/svg/mod.rs
@@ -1,7 +1,7 @@
-use std::fmt::format;
+use std::collections::HashMap;
 
-use chrono::{DateTime, Datelike, Days, Local, Months, TimeZone, Utc, Weekday};
-use time::{Date, Month, Time};
+use chrono::{DateTime, Datelike, Days, Months, TimeZone, Weekday};
+use time::Date;
 
 use crate::types::ContributionActivity;
 
@@ -43,6 +43,7 @@ const DAY_SPACE: usize = 1;
 const DAY_SIZE: usize = 14;
 const DAY_SIZE_WITH_SPACE: usize = DAY_SIZE + DAY_SPACE * 2;
 const FIRST_DAY_OF_WEEK: Weekday = Weekday::Mon;
+const SVG_HEIGHT: usize = 140;
 
 const MONTH_NAMES: [&str; 12] = [
     "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
@@ -83,48 +84,91 @@ impl SvgRenderer {
             day = day.checked_add_days(Days::new(1)).unwrap();
         }
 
-        let content = result.iter().enumerate().map(|(week, day_elements)| {
-            let x = DAY_SIZE_WITH_SPACE * week + 1 + DAY_SIZE_WITH_SPACE;
-            // todo: fill elements into the <g> element
-            dbg!(format!(
-                r#"<g transform="translate({}, 18)" data-testid="user-contrib-cell-group"></g>"#,
-                x
-            ))
-        }).collect::<Vec<_>>().join("\n");
+        let content = Self::render_month_rows(result);
 
-        Self::wrap_svg(activity, group, &content)
+        let width = (group + 1) * DAY_SIZE_WITH_SPACE; // TODO: handle extra padding case (getExtraWidthPadding)
+        Self::wrap_svg(width, &content)
     }
 
-    fn wrap_svg(activity: &ContributionActivity, width: usize, content: &str) -> String {
-        let width = (width + 1) * DAY_SIZE_WITH_SPACE; // TODO: handle extra padding case (getExtraWidthPadding)
-        const HEIGHT: u16 = 140;
-
+    fn wrap_svg(width: usize, content: &str) -> String {
         format!(
-            r#"<svg width="{}" height="{}" class="contrib-calendar" data-testid="contrib-calendar">
+            r#"<svg xmlns="http://www.w3.org/2000/svg" width="{}" height="{}" class="contrib-calendar" data-testid="contrib-calendar">
     {}
     {}
 </svg>"#,
-            width, HEIGHT, STYLE, content
+            width, SVG_HEIGHT, STYLE, content
         )
+    }
+
+    fn render_month_rows(result: Vec<Vec<Data>>) -> String {
+        let content = result.into_iter().enumerate().map(|(week, day_elements)| {
+            let x = DAY_SIZE_WITH_SPACE * week + 1 + DAY_SIZE_WITH_SPACE;
+            let week_day_cells = Self::render_week_day_cells(day_elements);
+            dbg!(format!(
+                r#"<g transform="translate({}, 18)" data-testid="user-contrib-cell-group">{}</g>"#,
+                x,
+                week_day_cells
+            ))
+        }).collect::<Vec<_>>().join("\n");
+        content
+    }
+
+    fn render_week_day_cells(days: Vec<Data>) -> String {
+        const CELL_SIZE: usize = 14;
+        const CELL_RADIUS: usize = 2;
+        const FIST_DAY_OF_WEEK: usize = 0; // todo
+
+        days.into_iter()
+            .map(|day| {
+                let (data_level, hover_info )  = Self::get_contrib_level(day.count);
+                let y = DAY_SIZE_WITH_SPACE * ((day.date.weekday().number_days_from_monday() as usize + 7 - FIST_DAY_OF_WEEK) % 7);
+
+                format!(r#"<rect x="0" y="{y}" rx="{CELL_RADIUS}" ry="{CELL_RADIUS}" width="{CELL_SIZE}" height="{CELL_SIZE}" data-level="{data_level}" data-hover-info="{hover_info}" class="user-contrib-cell has-tooltip" data-testid="user-contrib-cell" data-html="true" data-container="body"></rect>"#)
+            })
+            .collect::<Vec<_>>()
+            .join("\n")
+    }
+
+    fn get_contrib_level(count: usize) -> (usize, String) {
+        let contrib_levels = HashMap::from([
+            (30, (4, "30+ contributions".into())),
+            (20, (3, "20-29 contributions".into())),
+            (10, (2, "10-19 contributions".into())),
+            (1, (1, "1-9 contributions".into())),
+            (0usize, (0, "No contributions".into())),
+        ]);
+
+        contrib_levels
+            .into_iter()
+            .find(|(min, _)| count >= *min)
+            .unwrap()
+            .1
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     use crate::{
-        provider::gitlab::Gitlab, provider::GitProvider, source::FixtureDataSource, svg::STYLE,
+        provider::{github::Github, GitProvider},
+        source::FixtureDataSource,
+        svg::STYLE,
     };
 
     use super::SvgRenderer;
 
     #[tokio::test]
     async fn basic() {
-        let activity = Gitlab::fetch(FixtureDataSource {}, "".into())
+        let activity = Github::fetch(FixtureDataSource {}, "".into())
             .await
             .unwrap();
 
         let today = chrono::offset::Local::now();
         let svg = SvgRenderer::render(&activity, today);
+
+        fs::write("/tmp/activity.svg", &svg).unwrap(); // todo: remove
+
         assert_eq!(
             &svg,
             &format!(

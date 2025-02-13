d457bee064172c24fafed43259717aa465a6f7a9
Thomas Zahner <thomas.zahner@protonmail.ch>
2025-02-13 21:06:08 +0100

    Update flake to support 'nix build'

--- flake.nix
+++ flake.nix
@@ -19,29 +19,53 @@
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
-      devShells = forAllSystems (system: {
-        default =
-          let
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = [ (import rust-overlay) ];
-            };
-            rustVersion = "latest"; # using a specific version: "1.62.0"
-            rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
-              extensions = [
-                "rust-src" # for rust-analyzer
-                "rust-analyzer" # usable by IDEs like zed-editor
-                "clippy"
-              ];
-            };
-          in
-          pkgs.mkShell {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+          };
+          rustVersion = "latest"; # using a specific version: "1.62.0"
+          rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+            extensions = [
+              "rust-src" # for rust-analyzer
+              "rust-analyzer" # usable by IDEs like zed-editor
+              "clippy"
+            ];
+          };
+        in
+        {
+          default = pkgs.mkShell {
             packages = [
               pkgs.pkg-config
               pkgs.openssl
               rust
             ];
           };
-      });
+        }
+      );
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            name = "commitoria-web";
+            srcs = [
+              ./lib
+              ./web
+            ];
+            sourceRoot = "./web";
+            cargoDeps = pkgs.rustPlatform.importCargoLock {
+              lockFile = ./web/Cargo.lock;
+            };
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [ pkgs.openssl ];
+          };
+        }
+      );
     };
 }

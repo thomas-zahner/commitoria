436636a1fd15b7871036d53444e5ddc35bcc7367
Thomas Zahner <thomas.zahner@protonmail.ch>
2025-03-18 22:04:50 +0100

    Test idea & add todo

---
+++ TODO.md
@@ -0,0 +1,5 @@
+GitHub CI either with:
+
+- Crane: https://nix.dev/manual/nix/2.24/command-ref/new-cli/nix3-flake-check.html
+- Current but updated Flake (doing Cargo test), see: https://nix.dev/manual/nix/2.24/command-ref/new-cli/nix3-flake-check.html
+- Or doing plain Rust/Cargo builds & tests
--- flake.nix
+++ flake.nix
@@ -1,89 +1,25 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
+  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.mkLib pkgs;
+      in
     {
-      nixpkgs,
-      rust-overlay,
-      self,
-      ...
-    }:
-    let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ (import rust-overlay) ];
-          };
-          rustVersion = "latest";
-          rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
-            extensions = [
-              "rust-src" # for rust-analyzer
-              "rust-analyzer" # usable by IDEs like zed-editor
-              "clippy"
-            ];
-          };
-        in
-        {
-          default = pkgs.mkShell {
-            packages = [
-              pkgs.pkg-config
-              pkgs.openssl
-              rust
-            ];
-          };
-        }
-      );
+      packages.default = craneLib.buildPackage {
+        src = craneLib.cleanCargoSource ./web;
 
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.rustPlatform.buildRustPackage {
-            name = "commitoria-web";
-            meta.mainProgram = "web";
-            srcs = [
-              ./lib
-              ./web
-            ];
-            sourceRoot = "./web";
-            cargoDeps = pkgs.rustPlatform.importCargoLock {
-              lockFile = ./web/Cargo.lock;
-            };
-            nativeBuildInputs = [ pkgs.pkg-config ];
-            buildInputs = [ pkgs.openssl ];
-          };
-
-          docker = pkgs.dockerTools.buildImage {
-            name = "commitoria-web";
-
-            copyToRoot = pkgs.buildEnv {
-              name = "image-root";
-              paths = [
-                pkgs.openssl
-                pkgs.cacert
-              ];
-            };
-
-            config = {
-              Cmd = [ "${self.packages.${pkgs.system}.default}/bin/web" ];
-            };
-          };
-        }
-      );
-    };
+        # Add extra inputs here or any other derivation settings
+        # doCheck = true;
+        # buildInputs = [];
+        # nativeBuildInputs = [];
+      };
+    });
 }
+

c6597ee22062ae785068ad3b1b4cc7ffb1b4d479
Thomas Zahner <thomas.zahner@protonmail.ch>
2025-04-16 22:22:42 +0200

    Delete repository path after extracting data

--- lib/src/provider/git.rs
+++ lib/src/provider/git.rs
@@ -1,10 +1,28 @@
 use super::{GitProvider, Result};
-use crate::types::{ContributionActivity, YEAR};
+use crate::types::{ContributionActivity, Error, YEAR};
 use chrono::DateTime;
 use git2::{build::RepoBuilder, FetchOptions, Sort};
-use std::{collections::BTreeMap, path::Path};
+use std::{collections::BTreeMap, path::PathBuf};
+use uuid::Uuid;
 
 pub struct Git {}
+
+struct TemporaryPath(PathBuf);
+
+impl TemporaryPath {
+    fn new() -> Self {
+        let path_string = format!("/tmp/{}", Uuid::new_v4());
+        Self(PathBuf::from(path_string))
+    }
+}
+
+impl Drop for TemporaryPath {
+    fn drop(&mut self) {
+        if let Err(e) = std::fs::remove_dir_all(&self.0) {
+            eprintln!("{e}");
+        }
+    }
+}
 
 impl GitProvider for Git {
     async fn fetch<S: crate::source::DataSource>(
@@ -20,7 +38,8 @@ impl GitProvider for Git {
         let mut builder = RepoBuilder::new();
         builder.fetch_options(options).bare(true);
 
-        let repo = builder.clone(url, Path::new("/tmp/cloned-repository"))?;
+        let repository_path = TemporaryPath::new();
+        let repo = builder.clone(url, &repository_path.0)?;
         let mut revwalk = repo.revwalk()?;
 
         revwalk.set_sorting(Sort::TIME)?;
@@ -33,7 +52,7 @@ impl GitProvider for Git {
             let rev = *rev.as_ref()?;
             let commit = repo.find_commit(rev)?;
             let commit_time = DateTime::from_timestamp(commit.time().seconds(), 0)
-                .ok_or(super::Error::UnableToParseDate(
+                .ok_or(Error::UnableToParseDate(
                     "Invalid timestamp encountered".into(),
                 ))?
                 .date_naive();
